e80f24030a2c28fb37c6a9f175b5701ccb85e9cc
colton <adamscolton64@gmail.com>
2023-01-23 11:51:43 -0600

    Strings | NegatePhrase Complete

--- NegatePhrase.java
+++ NegatePhrase.java
@@ -1,5 +1,9 @@
 public class NegatePhrase {
     public static void main(String[] args) {
-        
+        if (args[0].indexOf('n') == 0 && args[0].indexOf('o') == 1 && args[0].indexOf('t') == 2) {
+            System.out.println(args[0]);
+        } else {
+            System.out.println("not " + args[0]);
+        }
     }
 }
