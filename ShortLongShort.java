052bf050861adf7510fbc0991ab31835ebdfd053
colton <adamscolton64@gmail.com>
2023-01-23 11:43:53 -0600

    Strings | ShortLongShort Completed

--- ShortLongShort.java
+++ ShortLongShort.java
@@ -1,5 +1,18 @@
 public class ShortLongShort {
     public static void main(String[] args) {
-        
+        String first = new String("");
+        String second = new String("");
+        if (args[0].length() == args[1].length()) {
+            System.out.println("The Statements are the same length. Please try again");
+        } else {
+            if (args[0].length() < args[1].length()) {
+                first = new String(args[0]);
+                second = new String(args[1]); 
+            } else {
+                first = new String(args[1]);
+                second = new String(args[0]);
+            }
+            System.out.println(args[0] + " | " + args[1] + " | " + first + second + first);
+        }
     }
 }
