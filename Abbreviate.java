b72290bf1ad239ec5aa0b10f7ef07a95988effb1
colton <adamscolton64@gmail.com>
2023-01-23 12:01:14 -0600

    Strings | Abbreviate Complete

--- Abbreviate.java
+++ Abbreviate.java
@@ -1,5 +1,11 @@
 public class Abbreviate {
     public static void main(String[] args) {
-        
+        if ( args[0].length() <= 8) {
+            System.out.println(args[0]);
+        } else {
+            String replace = new String(args[0].substring(2, args[0].length() - 2));
+            System.out.println(args[0].replace(replace, "..."));
+        }
+    
     }
 }
