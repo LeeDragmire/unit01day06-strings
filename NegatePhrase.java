5fe33a5f31b2d20cfb110a96a408a658bd430ae5
colton <adamscolton64@gmail.com>
2023-02-01 11:28:13 -0600

    Strings | NegatePhrase Fixed

--- NegatePhrase.java
+++ NegatePhrase.java
@@ -1,9 +1,13 @@
 public class NegatePhrase {
     public static void main(String[] args) {
-        if (args[0].indexOf('n') == 0 && args[0].indexOf('o') == 1 && args[0].indexOf('t') == 2) {
-            System.out.println(args[0]);
+        String negated;
+        if (args[0].indexOf("not") == 0) {
+            negated = args[0];
         } else {
-            System.out.println("not " + args[0]);
+            negated = "not " + args[0];
         }
+        System.out.println(negated);
     }
 }
+
+// javac NegatePhrase.java; java NegatePhrase 
