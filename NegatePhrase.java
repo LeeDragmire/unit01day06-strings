069104e4bb829fcbc62316ed42d188ddbbed451f
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-31 19:34:02 -0600

    NegatePhrase.java feedback

--- NegatePhrase.java
+++ NegatePhrase.java
@@ -1,3 +1,5 @@
+// this works, but substring (vs indexOf) would be a more natural way to compare the start to "not"
+// TODO: this assignment asked you to actually create a new String (i.e. define it in a variable), and not just print a result
 public class NegatePhrase {
     public static void main(String[] args) {
         if (args[0].indexOf('n') == 0 && args[0].indexOf('o') == 1 && args[0].indexOf('t') == 2) {
